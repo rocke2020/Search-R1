3a768da965fa5e82b296023cf02ddc98d65fb8e2
qichang.dong <dongqichang@ecmas.ai>
2025-05-04 22:26:54 +0800

    u

--- retrieval_launch.sh
+++ retrieval_launch.sh
@@ -1,13 +1,10 @@
 
-file_path=/data/comm/wiki-18
+file_path=/data/corpus/wiki-18
 index_file=$file_path/e5_Flat.index
 corpus_file=$file_path/wiki-18.jsonl
 retriever_name=e5
 retriever_path=/data/model/intfloat/e5-base-v2
 gpu=0,1
-if [ -z $gpu ]; then
-    gpu=0
-fi
 export CUDA_VISIBLE_DEVICES=$gpu
 
 python search_r1/search/retrieval_server.py --index_path $index_file \
